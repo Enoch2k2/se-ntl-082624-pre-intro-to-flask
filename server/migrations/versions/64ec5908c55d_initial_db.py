c91dc2bf360fb1305c55c207ceab7de1e442c1d8
Enoch2k2 <enoch2k2@hotmail.com>
2024-10-25 14:22:34 -0500

    initial commit

---
+++ server/migrations/versions/64ec5908c55d_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 64ec5908c55d
+Revises: 
+Create Date: 2024-10-25 14:21:50.490879
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '64ec5908c55d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
